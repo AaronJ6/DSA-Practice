790325599f780c6ff0b9271dc76cca185b6a118a
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-10 08:08:25 +0400

    First Commit

---
+++ twosum_II.py
@@ -0,0 +1,18 @@
+def twoSum(self, numbers, target):
+    """
+    :type numbers: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    l = 0
+    r = len(numbers)-1
+    op = set()
+    while(l<r):
+        chk = numbers[l] + numbers[r]
+        if(chk<target): l+=1
+        elif(chk>target): r-=1
+        else:
+            op.add(l+1)
+            op.add(r+1)
+            break
+    return(op)

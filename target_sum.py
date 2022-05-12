2e6f3fa0c05ef6e502f8b4583d89b2fa6a049554
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-12 09:04:32 +0400

    v1.0

--- target_sum.py
+++ target_sum.py
@@ -1,11 +1,33 @@
-# def findTargetSumWays(self, nums, target):
-#     """
-#     :type nums: List[int]
-#     :type target: int
-#     :rtype: int
-#     """
-#     dp = {} # position - (index, total) -> storing number of ways #!type(dp) = dictionary
+from collections import defaultdict
 
-#     def backtrack(i, total): #imp concept - backtracking, dynamic programming with caching - thats why we keep a dictionary to store the possibilities of different combinations
+def findTargetSumWays(self, nums, target): #!NEETCODE method
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    dp = {} # position - (index, total) -> storing number of ways #!type(dp) = dictionary
 
-#         if(i == len(nums)): return 1 if total == target else 0
+    def backtrack(i, total): #imp concept - backtracking, dynamic programming with caching - thats why we keep a dictionary to store the possibilities of different combinations
+
+        if(i == len(nums)): return 1 if total == target else 0
+        if(i,total) in dp:
+            return dp[(i,total)]
+        dp[(i,total)] = backtrack(i+1, total + nums[i]) + backtrack(i+1, total - nums[i])
+        return(dp[(i,total)])
+
+    return(backtrack(0,0))
+
+
+    dp = defaultdict(int) #!COMMENT section answer - this builds the decision tree on a dictionary and gets the answer
+    dp[0] = 1
+
+    for num in nums:
+        new_dp = defaultdict(int)
+        print(dp)
+        for n in dp:
+            print(n)
+            new_dp[n+num] += dp[n]
+            new_dp[n-num] += dp[n]
+            print(new_dp)
+        dp = new_dp

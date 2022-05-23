46eb7344a3156e091d693f56715ce05f1add19fb
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-23 07:09:02 +0400

    v1.0

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -4,15 +4,21 @@ def isValid(s):
     :rtype: bool
     """
     stk = []
+    if(len(s)<2):
+        return False
     for i in range(len(s)):
         if s[i] == '(' or s[i] == '[' or s[i] == '{':
             stk.append(s[i])
         else:
             chk = stk.pop()
-            if s[i] != chk:
+            if s[i] == ')' and chk != '(':
+                return False
+            elif s[i] == ']' and chk != '[':
+                return False
+            if s[i] == '}' and chk != '{':
                 return False
     return True
 
-s = '()'
+s = '(]'
 ck = isValid(s)
 print(ck)

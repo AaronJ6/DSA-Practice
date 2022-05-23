b00a02a9d85a073d4438d7526c95fe154cd9b254
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-23 07:18:35 +0400

    v1.2

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -5,17 +5,22 @@ def isValid(s):
     """
     stk = []
     i = 0
-    while i<len(s) and len(stk)>0:
+    stk.append(s[i])
+    i+=1    
+    while i<len(s):
         if s[i] == '(' or s[i] == '[' or s[i] == '{':
             stk.append(s[i])
             i+=1
         else:
             chk = stk.pop()
-            if s[i] == ')' and chk != '(':
-                return False
-            elif s[i] == ']' and chk != '[':
-                return False
-            if s[i] == '}' and chk != '{':
+            if chk != None:
+                if s[i] == ')' and chk != '(':
+                    return False
+                elif s[i] == ']' and chk != '[':
+                    return False
+                elif s[i] == '}' and chk != '{':
+                    return False
+            else:
                 return False
             i+=1
     if len(stk)>0:
@@ -23,6 +28,6 @@ def isValid(s):
     else:
         return True
 
-s = '(]'
+s = '(){}}{'
 ck = isValid(s)
 print(ck)

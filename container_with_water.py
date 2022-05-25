c2925b8a68c976374762699cf4139777dcfa688d
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-25 21:12:55 +0400

    v1.0

--- container_with_water.py
+++ container_with_water.py
@@ -1,26 +1,24 @@
+from tempfile import tempdir
+
+
 def maxArea(height):
     """
     :type height: List[int]
     :rtype: int
     """
-    if len(height) == 2:
-        return(height[0]*height[1])
-    
-    breadth = 1
-    i, j = 1, 0
+    if len(height) == 2: #!BRUTE FORCE
+        return(min(height[0],height[1]))
+
     area = 0
 
-    while i<len(height):
-        ht = min(height[0], height[i])
-        temp = ht*breadth
-        area = max(area,temp)
-        i+=1
-        breadth+=1
-    breadth-=1
-    while j<i:
-        ht = min(height[j], height[i-1])
-        temp = ht*breadth
-        area = max(area,temp)
-        j+=1
-        breadth-=1
+    for l in range(len(height)):
+        for r in range(l+1,len(height)):
+            temp = (r-l)*(min(height[l],height[r]))
+            area = max(area,temp)
+    
     return(area)
+
+
+
+height = [2,3,4,5,18,17,6]
+print(maxArea(height))

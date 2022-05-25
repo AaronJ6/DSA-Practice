d6a50d520f59c5bfff7cc4c27e2cd95ed26e803b
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-25 08:14:39 +0400

    First Commit

---
+++ same_tree.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if p is None:
+            return False
+        if q is None:
+            return False
+        
+        Q1 = deque([p])
+        Q2 = deque([q])
+        chk = 0
+        
+        while Q1 and Q2:
+            l1 = Q1.popleft()
+            l2 = Q2.popleft()
+            
+            if l1 and l2:
+                if l1.val == l2.val:
+                    Q1.append(l1.left)
+                    Q1.append(l1.right)
+                    Q2.append(l2.left)
+                    Q2.append(l2.right)
+                else:
+                    chk = 1
+                    break
+            elif l1 or l2:
+                chk = 1
+                break
+        if chk == 1:
+            return False
+        else:
+            return True

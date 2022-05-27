d32b4a9c5003110634fc889d402682bf53380121
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-27 07:27:19 +0400

    v2.0

--- coin_change.py
+++ coin_change.py
@@ -21,3 +21,19 @@ def coinChange(coins, amount): #!MY CODE - WRONG
         return count
     else:
         return -1
+
+def coinChange(coins, amount): #!NEETCODE
+    """
+    :type coins: List[int]
+    :type amount: int
+    :rtype: int
+    """
+    dp = [amount+1] * (amount+1)
+    dp[0] = 0
+    
+    for a in range(1,amount+1):
+        for c in coins:
+            temp = a-c
+            if temp>=0:
+                dp[a] = min(dp[a],1+dp[temp])
+    return dp[amount] if dp[amount]!=amount+1 else -1

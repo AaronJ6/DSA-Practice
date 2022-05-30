f09b6f04e4d23f5da7c60f77b2701233b8dd9e91
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-31 01:20:27 +0400

    v2.0

--- longest_inc_subseq.py
+++ longest_inc_subseq.py
@@ -3,8 +3,14 @@ def lengthOfLIS(nums):
     :type nums: List[int]
     :rtype: int
     """
-    # sz = 0
-    dp = [0]*(len(nums)-1)
+    dp = [1]*len(nums) #!NEETCODE
+    for i in range(len(nums)-1, -1, -1):
+        for j in range(i+1, len(nums)):
+            if nums[i]<nums[j]:
+                dp[i] = max(dp[i], 1+dp[j])
+    return max(dp)
+    """ 
+    dp = [0]*len(nums) #!MY CODE
     dp[0] = 1
     i = 1
     for n in nums[1:]:
@@ -16,7 +22,7 @@ def lengthOfLIS(nums):
             # else:
             dp[i] = dp[i-1]
         i+=1
-    return dp[-1]
+    return dp[-1] """
 
 nums = [0,1,0,3,2,3]
 print(lengthOfLIS(nums))

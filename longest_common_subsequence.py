5ebce76f1bc3d217420d0a7f35b9da129e92c173
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-05-30 20:51:47 +0400

    v1.0

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -4,7 +4,20 @@ def longestCommonSubsequence(text1, text2):
     :type text2: str
     :rtype: int
     """
-    check = ""
+    #!NEETCODE - 2D DP - bottom up problem
+
+    dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+
+    for i in range(len(text1)-1, -1, -1):
+        for j in range(len(text2)-1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i+1][j+1]
+            else:
+                dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+
+    return dp[0][0]
+
+"""     check = "" #!MY CODE - Wrong
     num = 0
     j=0
     for i in range(len(text1)):
@@ -14,7 +27,7 @@ def longestCommonSubsequence(text1, text2):
 
     # if check == text2:
     num = len(check)
-    return num
+    return num """
 
 text1 = "abcde"
 text2 = "ace"

a29a06308814d0098ff7a67494ff0b3eef52021b
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-01 16:05:47 +0400

    Test Commit

--- test.py
+++ test.py
@@ -1,31 +1,18 @@
-# Python program to
-# demonstrate stack implementation
-# using collections.deque
+def inversionCount(arr, n):
+    # Your Code Here
+    rev = 0
+    for i in range(n):
+        min = arr[i]
+        post = i
+        for j in range(i+1,n):
+            if arr[j]<min:
+                min = arr[j]
+                post = j
+        if min != arr[i]:
+            arr[post], arr[i] = arr[i], arr[post]
+            rev+=1
+    return rev
 
-from collections import deque
-
-stack = deque()
-
-# append() function to push
-# element in the stack
-stack.append('a')
-stack.append('b')
-stack.append('c')
-
-print('Initial stack:')
-print(stack)
-
-# pop() function to pop
-# element from stack in
-# LIFO order
-print('\nElements popped from stack:')
-print(stack.popleft())
-print(stack.popleft())
-print(stack.popleft())
-
-print('\nStack after elements are popped:')
-print(stack)
-
-# uncommenting print(stack.pop())
-# will cause an IndexError
-# as the stack is now empty
+arr = [2,4,1,3,5]
+op = inversionCount(arr, len(arr))
+print(op)

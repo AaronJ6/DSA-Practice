dbbaab05b3f2a53b9b71ea2a34f9bb0f640efb87
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-10 16:12:09 +0400

    v1.0

--- decode_ways.py
+++ decode_ways.py
@@ -2,7 +2,7 @@ def rec_fn(i, s):
     if i >= len(s):
         return 0
     else:
-        lng1, lng2 = 0, 0
+        lng1, lng2 = 1, 1
         num1 = int(s[i])
         if i+1<len(s):
             num2 = int(s[i])
@@ -10,10 +10,14 @@ def rec_fn(i, s):
         else:
             num2 = 0
         if num1>0 and num1<27:
-            lng1 = 1+rec_fn(i+1,s)
+            lng1 += rec_fn(i+1,s)
+        else:
+            lng1 = -1
         if num2>0 and num2<27:
-            lng2 = 1+rec_fn(i+2,s)
+            lng2 += rec_fn(i+2,s)
+        else:
+            lng2 = -1
         return max(lng1, lng2)
 
-s = "2206"
+s = "06"
 print(rec_fn(0,s))

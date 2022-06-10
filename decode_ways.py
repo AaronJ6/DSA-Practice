620bee96fe680fef83fe725f7c10b2ab13b30eea
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-10 19:35:04 +0400

    v2.0

--- decode_ways.py
+++ decode_ways.py
@@ -1,8 +1,27 @@
+def numDecodings(s): #!NEETCODE
+    """
+    :type s: str
+    :rtype: int
+    """
+    dp = {len(s) : 1}
+    
+    def dfs(i):
+        if i in dp:
+            return dp[i]
+        if s[i] == "0":
+            return 0
+        res = dfs(i+1)
+        if i+1<len(s) and (s[i] == '1' or (s[i] == '2' and s[i+1] in "0123456")):
+            res+=dfs(i+2)
+        dp[i] = res
+        return res
+    return dfs(0)
+
 def rec_fn(i, s):
     if i >= len(s):
         return 0
     else:
-        lng1, lng2 = 1, 1
+        lng1, lng2 = 1, 1 #!BRUTE FORCE
         num1 = int(s[i])
         if i+1<len(s):
             num2 = int(s[i])

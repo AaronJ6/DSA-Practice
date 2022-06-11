73329cc95401a1fbf9e20654df821e7cc66a99b0
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-11 12:47:41 +0400

    v1.0

--- unique_path.py
+++ unique_path.py
@@ -4,7 +4,7 @@ def uniquePaths(m, n):
     :type n: int
     :rtype: int
     """
-    dp = [[0 for i in range(n)] for j in range(m)]
+    dp = [[0 for i in range(n)] for j in range(m)] #!MY CODE WITH REC+MEMOIZATION
     dp[m-1][n-1] = 1
     # print(path)
     def rec_fn(i,j):
@@ -21,4 +21,20 @@ def uniquePaths(m, n):
             return 0
     
     return rec_fn(0,0)
+
+def uniquePaths(m, n):
+    """
+    :type m: int
+    :type n: int
+    :rtype: int
+    """
+    row = [1]*n #*bottom row initialised as 1
+    
+    for i in range(m-1):#*going through all rows except the last
+        newRow = [1]*n #*the current row
+        for j in range(n-2, -1, -1): #*n-2 because the last column is always 1
+            newRow[j] = newRow[j+1] + row[j]  #*newRow[j+1] is the for taking the val from next column and row is for the row below it
+        row = newRow #*that is why we update row with newRow since we want row to be having the vals of the row below current(newRow)
+    
+    return row[0]
         

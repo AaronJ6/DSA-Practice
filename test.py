9c1d488b7abffced508d24749bb864d73dae325d
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-14 08:31:50 +0400

    Test no_of_islands

--- test.py
+++ test.py
@@ -1,18 +1,47 @@
-def inversionCount(arr, n):
-    # Your Code Here
-    rev = 0
-    for i in range(n):
-        min = arr[i]
-        post = i
-        for j in range(i+1,n):
-            if arr[j]<min:
-                min = arr[j]
-                post = j
-        if min != arr[i]:
-            arr[post], arr[i] = arr[i], arr[post]
-            rev+=1
-    return rev
+def find_island(m, n, i, j, grid):
+    if (i+1 <= (m-1)):
+        if (grid[i+1][j]=='1'):
+            grid[i+1][j] = '2'
+            find_island(m,n,i+1,j,grid)
+    if (j+1 <= (n-1)):
+        if (grid[i][j+1]=='1'):
+            grid[i][j+1] = '2'
+            find_island(m,n,i,j+1,grid)
+    if (i-1 >= 0):
+        if (grid[i-1][j]=='1'):
+            grid[i-1][j] = '2'
+            find_island(m,n,i-1,j,grid)
+    if (j-1 >= 0):
+        if (grid[i][j-1]=='1'):
+            grid[i][j-1] = '2'
+            find_island(m,n,i,j-1,grid)
 
-arr = [2,4,1,3,5]
-op = inversionCount(arr, len(arr))
-print(op)
+
+
+def numIslands(grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+    number_of_islands = 0;
+    i = 0;
+    j = 0;
+    m = len(grid);
+    n = len(grid[0]);
+    while ((i <= m-1) or (j <= n-1)):
+        if (grid[i][j]=='0' or grid[i][j]=='2'):
+            if (j < n-1):
+                j = j+1;
+            elif ((j == (n-1)) and (i < (m-1))):
+                i = i+1;
+                j = 0;
+            elif ((j == (n-1)) and (i == (m-1))):
+                return number_of_islands
+        elif (grid[i][j]=='1'):
+            grid[i][j] = '2'
+            find_island(m,n,i,j,grid)
+            number_of_islands = number_of_islands + 1
+    return number_of_islands
+
+grid = [["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]
+print(numIslands(grid))

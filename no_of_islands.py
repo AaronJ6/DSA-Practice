6e9b81e8257276cc942306fc8ca22632a85a906f
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-14 08:22:25 +0400

    v1.0

--- no_of_islands.py
+++ no_of_islands.py
@@ -1,6 +1,6 @@
 import collections
 
-def numIslands(grid):
+def numIslands_bfs(grid):
     """
     :type grid: List[List[str]]
     :rtype: int
@@ -35,3 +35,27 @@ def numIslands(grid):
                 bfs(r,c,visit)
                 islands+=1
     return islands
+
+def numIslands_dfs(grid):
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+    def part(i, j, grid):
+        if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j]!= '1':
+            return
+        else:
+            grid[i][j] = '0'
+        part(i, j+1, grid)
+        part(i,j-1,grid)
+        part(i+1, j, grid)
+        part(i-1,j,grid)
+    def islands(grid):
+        iss = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    iss += 1
+                    part(i,j,grid)
+        return iss
+    return islands(grid)

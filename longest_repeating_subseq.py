5af508834652b7033059339f4d5ec3a60df77e76
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-21 11:44:55 +0400

    v0.1

--- longest_repeating_subseq.py
+++ longest_repeating_subseq.py
@@ -2,7 +2,7 @@ def LongestRepeatingSubsequence(str):
     # Code here
     n = len(str)
     dp = [[0 for i in range(n+1)] for j in range(n+1)]
-    
+    #!this top to bottom problem solving of the longest common subsequence
     for i in range(1,n+1):
         for j in range(1,n+1):
             if (str[i-1] == str[j-1] and i!=j):

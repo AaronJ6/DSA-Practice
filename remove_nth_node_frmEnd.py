571e01721ed34e133c8daa72497ce895a2dba0f6
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-23 08:14:14 +0400

    v1.0

--- remove_nth_node_frmEnd.py
+++ remove_nth_node_frmEnd.py
@@ -1,9 +1,33 @@
-def removeNthFromEnd(head, n):
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head, n): #!NEETCODE
     """
     :type head: ListNode
     :type n: int
     :rtype: ListNode
     """
+    if not head:
+        return None
+    dummy = ListNode(0,head)
+    left = dummy
+    right = head
+    
+    while n>0 and right:
+        right = right.next
+        n-=1
+        
+    while right:
+        left = left.next
+        right = right.next
+
+    left.next = left.next.next
+    return dummy.next
+
+""" def removeNthFromEnd(head, n): #!MY CODE
     if not head:
         return None
     temp = head
@@ -25,4 +49,5 @@ def removeNthFromEnd(head, n):
             temp.next = temp.next.next
             return head
         temp = temp.next
-        i+=1
+        i+=1 
+ """

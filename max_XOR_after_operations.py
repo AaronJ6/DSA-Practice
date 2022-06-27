7a81612e6517f5ac2349ae8c875ff217b29b7963
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-27 17:16:14 +0400

    v0.2

--- max_XOR_after_operations.py
+++ max_XOR_after_operations.py
@@ -17,3 +17,11 @@ def maximumXOR(nums):
 # So bascially in the answer you get a 0 where all numbers in array have the bit place values 0, as there is no way to make it into a 1 .
 # Rest can be made into a 1, as atleast one number in the array has a bit place value 1.
 # And obviously you can check if atleast one of the element has a place value 1 by doing a cumulative OR.
+
+# nums[i] & (nums[i]^x) cannot be more than nums[i] because we are taking AND.
+# So this implies that for any number we can set the bits OFF ***(only the bits which are already ON (1))***.
+# BUT we cannot set any bitONif it were OFFalready .
+
+# Therefore , we can say that we will set bits ON and OFF for all numbers 
+# in such a way that XOR of all the numbers is equal to OR of all numbers. 
+# Because if a bit is not on in any of the numbers ,there is no way we can get it on in our ans.

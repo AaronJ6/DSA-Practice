84aefc2142620ca42814c8580d5982c1a9b348c5
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-06-29 18:38:07 +0400

    v1.0

--- max_spliced_arr.py
+++ max_spliced_arr.py
@@ -1,4 +1,4 @@
-def maximumsSplicedArray(A, B):
+def simplified_maximumsSplicedArray(A, B): #!Simplified code of the below code
     """
     :type nums1: List[int]
     :type nums2: List[int]
@@ -11,3 +11,32 @@ def maximumsSplicedArray(A, B):
             res = max(res, cur)
         return res + sum(B)
     return max(kadane(A, B), kadane(B, A)) 
+
+# def maximumsSplicedArray(nums1, nums2): #!Kadane's Algo
+#     """
+#     :type nums1: List[int]
+#     :type nums2: List[int]
+#     :rtype: int
+#     """
+#     sum1 = sum(nums1)
+#     sum2 = sum(nums2)
+    
+#     ans = max(sum1, sum2)
+    
+#     chk1=chk2=max1=max2 = 0
+    #? Algo loop
+#     for i in range(len(nums1)):
+#         chk1 += (nums2[i]-nums1[i])
+#         chk2 += (nums1[i]-nums2[i])
+        
+#         max1 = max(max1, chk1)
+#         max2 = max(max2, chk2)
+        
+#         if chk1<0:
+#             chk1 = 0
+#         if chk2<0:
+#             chk2 = 0
+    
+#     ans = max(ans, sum1+max1, sum2+max2)
+    
+#     return ans

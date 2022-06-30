a4a0edf8761b178a140a1843d511117f4bf61a3b
Aaron Joseph <aaronjoseph2002@gmail.com>
2022-07-01 00:38:21 +0400

    v1.0

--- set_matrix_zeroes.py
+++ set_matrix_zeroes.py
@@ -23,4 +23,38 @@ def setZeroes(matrix): #* MY Code
     for i in col:
         for j in range(s_row):
             matrix[j][i] = 0
+
+#!NEETCODE - O(1) Space complexity
+            #!O(m+n) Time complexity
+def setZeroes(matrix):
+    """
+    :type matrix: List[List[int]]
+    :rtype: None Do not return anything, modify matrix in-place instead.
+    """
+    rowZero = False
+    s_row = len(matrix)
+    s_col = len(matrix[0])
+    
+    for r in range(s_row):
+        for c in range(s_col):
+            if matrix[r][c] == 0:
+                matrix[0][c] = 0
+                if r>0:
+                    matrix[r][0] = 0
+                else:
+                    rowZero = True
+    
+    for r in range(1,s_row):
+        for c in range(1,s_col):
+            if matrix[0][c]==0 or matrix[r][0]==0:
+                matrix[r][c] = 0
+    
+    if matrix[0][0] == 0:
+        for r in range(s_row):
+            matrix[r][0] = 0
+    
+    if rowZero:
+        for c in range(s_col):
+            matrix[0][c] = 0
+                
     
